052c3fa77602916cf8da0d7ba2f0385c78ae6ba4
Supun <supunsnnk@gmail.com>
2024-03-18 14:20:22 +0530

    change folder names and locations

--- Interface_with_Data_Team/src/main/java/com/example/demo/controller/ProductController.java
+++ Interface_with_Data_Team/src/main/java/com/example/demo/controller/ProductController.java
@@ -2,16 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ProductDTO;
 import com.example.demo.dto.ShopperProductListDTO;
-import com.example.demo.entity.Product;
-import com.example.demo.entity.Shopper;
-import com.example.demo.entity.ShopperProductRelevance;
 import com.example.demo.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
